b07d58a06db05865a5126b96f28ea0b6696d773e
Łukasz Kwinta <33606064+lkwinta@users.noreply.github.com>
2023-10-20 09:12:45 +0200

    Lab 1 do oceny (#3)
    * Initialize project
    
    * lab 1 done
    
    * Initialize project
    
    * rebase on main

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,22 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.*;
+
+public class World {
+    public static void main(String[] args){
+        System.out.println("Start");
+        run(OptionParser.parse(args));
+        System.out.println("Stop");
+    }
+
+    public static void run(MoveDirection[] directions){
+        for(MoveDirection direction : directions) {
+            System.out.printf("Zwierzak %s%n", switch(direction){
+                case FORWARD -> "idzie do przodu";
+                case BACKWARD -> "idzie do tyłu";
+                case LEFT -> "skręca w lewo";
+                case RIGHT -> "skręca w prawo";
+            });
+        }
+    }
+}
