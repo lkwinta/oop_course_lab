57e3bf0ae59d5f95cb6683e9ede2c15b7011f09b
Łukasz Kwinta <lukaszmind@gmail.com>
2023-11-11 16:39:42 +0100

    animal comments

--- oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -1,22 +1,44 @@
 package agh.ics.oop.model;
 
+/**
+ * Class representing Animal object that will be placed on WorldMap
+ * */
 public class Animal {
     private MapDirection orientation;
     private Vector2d position;
 
+    /**
+     * Default constructor, initializes Animal in position (x,y) = (2,2) and North orientation
+     * */
     public Animal(){
         this(new Vector2d(2, 2));
     }
 
+    /**
+     * Constructor that sets new Animal at specified position with North orientation
+     *
+     * @param position Desired position of the anima
+     * */
     public Animal(Vector2d position){
         orientation = MapDirection.NORTH;
         this.position = position;
     }
 
+    /**
+     * Checks if current animal's position is specified one
+     *
+     * @param position Position to compare with current animal's position
+     **/
     public boolean isAt(Vector2d position){
         return this.position.equals(position);
     }
 
+    /**
+     * If possible moves animal with desired move direction
+     *
+     * @param direction {@link MoveDirection} with which animal will try to move
+     * @param moveValidator {@link IMoveValidator} checks if desired move is allowed on the {@link IWorldMap}
+     * */
     public void move(MoveDirection direction, IMoveValidator<Vector2d> moveValidator){
         switch(direction) {
             case LEFT       -> orientation = orientation.previous();
@@ -36,14 +58,23 @@ public class Animal {
         }
     }
 
+    /**
+     * Getter for orientation attribute
+     * */
     public MapDirection getOrientation() {
         return orientation;
     }
 
+    /**
+     * Getter for position attribute
+     */
     public Vector2d getPosition() {
         return position;
     }
 
+    /**
+     * @return Current animals orientation arrow
+     * */
     @Override
     public String toString(){
         return switch(orientation){
